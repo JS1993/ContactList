4c18637310fa87f93dc7277924af06acead0a0ae
susu <28025920@qq.com>
2016-04-20 21:25:47 +0800

    Add delete and edit  function .

--- ContactList/ContactList/ContactsListViewController.m
+++ ContactList/ContactList/ContactsListViewController.m
@@ -41,6 +41,15 @@
     self.tableView.tableFooterView=[[UIView alloc]init];
 }
 
+- (IBAction)editButton:(UIBarButtonItem *)sender {
+    //点击编辑之后变成完成
+        [self.tableView setEditing:!self.tableView.editing animated:YES];
+        if (self.tableView.editing) {
+            sender.title=@"完成";
+        }else{
+            sender.title=@"编辑";
+        }
+}
 #pragma mark - Table view data source
 
 
@@ -123,39 +132,36 @@
     [self performSegueWithIdentifier:@"newContact" sender:nil];
 }
 
-/*
 // Override to support conditional editing of the table view.
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
     // Return NO if you do not want the specified item to be editable.
+    
     return YES;
 }
-*/
 
-/*
 // Override to support editing the table view.
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
     if (editingStyle == UITableViewCellEditingStyleDelete) {
         // Delete the row from the data source
+        [self.ContactLists removeObjectAtIndex:indexPath.row];
         [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+        [NSKeyedArchiver archiveRootObject:self.ContactLists toFile:filePath];
+        [self.tableView reloadData];
     } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+       
     }   
 }
-*/
 
-/*
 // Override to support rearranging the table view.
 - (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+    
 }
-*/
 
-/*
 // Override to support conditional rearranging of the table view.
 - (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
     // Return NO if you do not want the item to be re-orderable.
     return YES;
 }
-*/
 
 /*
 #pragma mark - Navigation
